c107936d5b8eea0b2ad7f36f0e5715354a3e1974
Anders Lundsgard <sssttn@scania.com>
2014-12-12 10:47:36 +0100

    Deploy script

---
+++ deploy/SwapDeploy.ps1
@@ -0,0 +1,6 @@
+﻿param(
+  [string]$sitename
+)
+
+Import-AzurePublishSettingsFile -PublishSettingsFile "C:\Azure\CWP-App-based messaging for FMP-12-10-2014-credentials.publishsettings"
+Switch-AzureWebsiteSlot -Name $sitename -Force
